40265d279764c8f9d197a06b67b6b37ca7648cde
Maxime Feron <feron.maxime@gmail.com>
2019-08-23 01:47:04 +0200

    [spider] Add deleuimmobilier.com

---
+++ scrapy_real_estate/scrapy_real_estate/spiders/deleuimmobilier_com.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import logging
+import scrapy
+import re
+import urllib
+
+
+class DeleuimmobilierComSpider(scrapy.Spider):
+    name = 'deleuimmobilier.com'
+    site_base = 'https://www.deleuimmobilier.com/'
+    fail = False
+
+    def start_requests(self):
+        urls = [
+            f'recherche',
+        ]
+
+        post_data = {
+            '_method': 'POST',
+            'data[Recherche][type]': '1',
+            'data[Recherche][prix]': self.config['offers']['min_price'],
+            'data[Recherche][secteurs]': '',
+            'data[Recherche][ref]': '',
+            'recherchebiens': 'Valider',
+        }
+
+        post_data_str = '&'.join([f'{urllib.parse.quote(k)}={v}' for k, v in post_data.items()])
+
+        for url in urls:
+            yield scrapy.Request(
+                url=f'{self.site_base}{url}',
+                method='POST',
+                callback=self.parse,
+                errback=self.errback,
+                body=post_data_str,
+                headers={'Content-Type': 'application/x-www-form-urlencoded'},
+            )
+
+    def parse(self, response):
+        for offer in response.css('div.bloc_acc_nouv_cont2'):
+            if offer.css('div.bloc_acc_vendu_title'):
+                continue
+
+            a_tag = offer.css('a')
+            if not a_tag:
+                continue
+
+            url = response.urljoin(a_tag.attrib['href'])
+            description = [
+                offer.css('div.bloc_acc_nouv_title::text').get(),
+                offer.css('div.bloc_acc_nouv_prix::text').get(),
+            ]
+
+            yield {
+                'url': url,
+                'description': ' - '.join(description),
+            }
+
+        next_page = None
+        for a_tag in response.css('a.result_pagination_next'):
+            if a_tag.attrib['title'] == 'Page suivante':
+                next_page = response.urljoin(a_tag.attrib['href'])
+                break
+
+        if next_page:
+            yield response.follow(next_page, callback=self.parse, errback=self.errback)
+
+    def errback(self, failure):
+        self.fail = True
